c05e296b71e7cee7e76f174852b28c0b0081b2ec
NoerNova <noernova666@gmail.com>
2025-03-20 03:29:37 +0700

    Example script to make model and test

---
+++ make_model_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+CHECKPOINT="shn_0.064_10015_103100"
+
+lstmtraining --stop_training --continue_from data/shn/checkpoints/$CHECKPOINT.checkpoint --traineddata data/shn/shn.traineddata --model_output data/shn.traineddata
+sudo cp data/shn.traineddata /usr/local/share/tessdata/
+ocrmypdf -l shn --force-ocr data/demo/testpdf.pdf data/demo/testpdf_out_15.pdf
