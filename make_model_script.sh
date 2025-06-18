c23bdb6c66c4220e407498eb8732831457957fb2
NoerNova <noernova666@gmail.com>
2025-06-18 14:59:55 +0700

    Update: model 17 make model script

--- make_model_script.sh
+++ make_model_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+CHEKCPOINT_DIR="checkpoints/best"
 CHECKPOINT="shn_0.034_17056_285400"
+TESSDIR="/opt/homebrew/share/tessdata"
 
-lstmtraining --stop_training --continue_from data/shn/checkpoints/$CHECKPOINT.checkpoint --traineddata data/shn/shn.traineddata --model_output data/shn.traineddata
-sudo cp data/shn.traineddata /usr/local/share/tessdata/
-ocrmypdf -l shn --force-ocr data/demo/testpdf.pdf data/demo/testpdf_out_15.pdf
+lstmtraining --stop_training --continue_from $CHEKCPOINT_DIR/$CHECKPOINT.checkpoint --traineddata $CHEKCPOINT_DIR/shn.traineddata --model_output data/shn.traineddata
+sudo cp data/shn.traineddata $TESSDIR/
+# ocrmypdf -l shn --force-ocr data/demo/testpdf.pdf data/demo/testpdf_out_15.pdf
